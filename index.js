3d0a04031c2bb61eb2f50723e1a57b3541dd1334
Ujjen Bania <ujjenms.bania@gmail.com>
2022-09-10 22:27:55 -0500

    feat: support new file creation and appending content to file

--- index.js
+++ index.js
@@ -6,31 +6,54 @@ const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
   try {
     const username = process.env.GITHUB_REPOSITORY.split("/")[0];
     const repo = process.env.GITHUB_REPOSITORY.split("/")[1];
+
+    const makeNewFile = core.getInput("newFile");
+    const overwriteContent = core.getInput("overwrite");
+    const userContent = core.getInput("content");
+
+    let sha = "";
+    let fileContent = "";
+
     const getOutputFile = await octokit
-      .request('GET /repos/{owner}/{repo}/contents/{path}', {
+      .request("GET /repos/{owner}/{repo}/contents/{path}", {
         owner: username,
         repo: repo,
-        path: core.getInput('path'),
+        path: core.getInput("path"),
       })
       .catch((e) => {
         console.error("Failed: ", e);
         core.setFailed("Failed: ", e.message);
       });
-    const sha = getOutputFile.data.sha;
 
-    const data = core.getInput('content');
+    if (getOutputFile === undefined) {
+      if (!makeNewFile) {
+        return;
+      }
+    } else {
+      sha = getOutputFile.data.sha;
+      fileContent = getOutputFile.data.content;
+    }
+
+    const dataToWrite = overwriteContent
+      ? `${fileContent} \n${userContent}`
+      : userContent;
+
+    const params = {
+      owner: username,
+      repo: repo,
+      path: core.getInput("path"),
+      message: "(Automated) Update file",
+      content: Buffer.from(dataToWrite, "utf8").toString("base64"),
+    };
+
+    if (sha != "" && overwriteContent) {
+      params.sha = sha;
+    }
 
     await octokit
-      .request('PUT /repos/{owner}/{repo}/contents/{path}', {
-        owner: username,
-        repo: repo,
-        path: core.getInput('path'),
-        message: "(Automated) Update file",
-        content: Buffer.from(data, "utf8").toString("base64"),
-        sha: sha,
-      })
+      .request("PUT /repos/{owner}/{repo}/contents/{path}", params)
       .then(() => {
-        core.setOutput('Content written: ', data);
+        core.setOutput("Content written: ", data);
       })
       .catch((e) => {
         console.error("Failed: ", e);
