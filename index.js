a2d66e41dcb7666d7146675352d3dd09bea10723
Ujjen Bania <ujjenms.bania@gmail.com>
2022-09-18 12:54:11 -0500

    feat: test code

--- index.js
+++ index.js
@@ -4,64 +4,65 @@ const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
 (async () => {
   try {
-    const username = process.env.GITHUB_REPOSITORY.split("/")[0];
-    const repo = process.env.GITHUB_REPOSITORY.split("/")[1];
+    console.error("It is running new code");
+  //   const username = process.env.GITHUB_REPOSITORY.split("/")[0];
+  //   const repo = process.env.GITHUB_REPOSITORY.split("/")[1];
 
-    const makeNewFile = core.getInput("newFile");
-    const overwriteOrAppend = core.getInput("overwrite");
-    const userContent = core.getInput("content");
+  //   const makeNewFile = core.getInput("newFile");
+  //   const overwriteOrAppend = core.getInput("overwrite");
+  //   const userContent = core.getInput("content");
 
-    let sha = "";
-    let fileContent = "";
+  //   let sha = "";
+  //   let fileContent = "";
 
-    const getOutputFile = await octokit
-      .request("GET /repos/{owner}/{repo}/contents/{path}", {
-        owner: username,
-        repo: repo,
-        path: core.getInput("path"),
-      })
-      .catch((e) => {
-        console.error("Failed: ", e);
-        core.setFailed("Failed: ", e.message);
-      });
+  //   const getOutputFile = await octokit
+  //     .request("GET /repos/{owner}/{repo}/contents/{path}", {
+  //       owner: username,
+  //       repo: repo,
+  //       path: core.getInput("path"),
+  //     })
+  //     .catch((e) => {
+  //       console.error("Failed: ", e);
+  //       core.setFailed("Failed: ", e.message);
+  //     });
 
-    if (getOutputFile === undefined) {
-      if (!makeNewFile) {
-        return;
-      }
-    } else {
-      sha = getOutputFile.data.sha;
-      fileContent = getOutputFile.data.content;
-    }
+  //   if (getOutputFile === undefined) {
+  //     if (!makeNewFile) {
+  //       return;
+  //     }
+  //   } else {
+  //     sha = getOutputFile.data.sha;
+  //     fileContent = getOutputFile.data.content;
+  //   }
 
-    const dataToWrite = overwriteOrAppend;
-      // overwriteOrAppend === "Append"
-      //   ? `${fileContent} \n${userContent}`
-      //   : userContent;
+  //   const dataToWrite = overwriteOrAppend;
+  //     // overwriteOrAppend === "Append"
+  //     //   ? `${fileContent} \n${userContent}`
+  //     //   : userContent;
 
-    const params = {
-      owner: username,
-      repo: repo,
-      path: core.getInput("path"),
-      message: "(Automated) Update file",
-      content: Buffer.from(dataToWrite, "utf8").toString("base64"),
-    };
+  //   const params = {
+  //     owner: username,
+  //     repo: repo,
+  //     path: core.getInput("path"),
+  //     message: "(Automated) Update file",
+  //     content: Buffer.from(dataToWrite, "utf8").toString("base64"),
+  //   };
 
-    if (sha != "" && makeNewFile) {
-      params.sha = sha;
-    }
+  //   if (sha != "" && makeNewFile) {
+  //     params.sha = sha;
+  //   }
 
-    await octokit
-      .request("PUT /repos/{owner}/{repo}/contents/{path}", params)
-      .then(() => {
-        core.setOutput("content", dataToWrite);
-      })
-      .catch((e) => {
-        console.error("Failed: ", e);
-        core.setFailed("Failed: ", e.message);
-      });
-  } catch (e) {
-    console.error("Failed: ", e);
-    core.setFailed("Failed: ", e.message);
-  }
+  //   await octokit
+  //     .request("PUT /repos/{owner}/{repo}/contents/{path}", params)
+  //     .then(() => {
+  //       core.setOutput("content", dataToWrite);
+  //     })
+  //     .catch((e) => {
+  //       console.error("Failed: ", e);
+  //       core.setFailed("Failed: ", e.message);
+  //     });
+  // } catch (e) {
+  //   console.error("Failed: ", e);
+  //   core.setFailed("Failed: ", e.message);
+  // }
 })();
